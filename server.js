3132a4585faabb8d659779046a004da80f385375
Noppadon019 <kotan.nam007@hotmail.com>
2018-09-17 09:38:33 +0700

    server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
+
