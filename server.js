66c9b6638211134139c92cfea7062c338aed250a
Noppadon019 <kotan.nam007@hotmail.com>
2018-09-17 10:17:56 +0700

    server

--- server.js
+++ server.js
@@ -1,26 +1,50 @@
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
+});
+
+//app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'dbuser',
-  password : 's3kreee7',
-  database : 'my_db'
-});
+// Display all product
 
+app.get('/student', function (req, res) {
 connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
+connection.query('select * from students', function (err, rows, fields) {
+        console.log('DATA' + rows);
+        res.render('pages/students', { students: rows })
+    
+    console.log(rows);
+    
 })
 
 connection.end()
+});
+
+app.get('/subjects', function (req, res) {
+    connection.connect()
+    
+    connection.query('select * from subjects', function (err, rows, fields) {
+            console.log('DATA' + rows);
+            res.render('pages/subjects', { subjects: rows })
+        
+        console.log(rows);
+        
+    })
+    
+    connection.end()
+    });
+    
+console.log('App is running at http://localhost:8080');
+app.listen(8080);
