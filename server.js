9ed7f8d9fd7dc9292070caa7336eb1aa87de2d24
Noppadon019 <kotan.nam007@hotmail.com>
2018-09-17 11:09:04 +0700

    1

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 
-var mysql = require('mysql')
+var mysql = require('mysql');
+var data;
+var data2;
 var connection = mysql.createConnection({
   host     : 'www.db4free.net',
   user     : 's140390',
@@ -18,33 +20,34 @@ app.get('/', function (req, res) {
 
 // Display all product
 
-app.get('/student', function (req, res) {
 connection.connect()
 
 connection.query('select * from students', function (err, rows, fields) {
-        console.log('DATA' + rows);
-        res.render('pages/students', { students: rows })
-    
-    console.log(rows);
+       data = rows;
     
 })
 
+connection.query('select * from subjects', function (err, rows, fields) {
+    data2 = rows;
+
+})
+
 connection.end()
+
+
+app.get('/students', function (req, res) {
+     
+    res.render('pages/students', { students: data })
+    
 });
 
-app.get('/subjects', function (req, res) {
-    connection.connect()
-    
-    connection.query('select * from subjects', function (err, rows, fields) {
-            console.log('DATA' + rows);
-            res.render('pages/subjects', { subjects: rows })
-        
-        console.log(rows);
-           connection.end()
-    })
-    
- 
+
+    app.get('/subjects', function (req, res) {
+
+        res.render('pages/subjects', { students: data2 })
     });
     
 console.log('App is running at http://localhost:8080');
+
+
 app.listen(8080);
